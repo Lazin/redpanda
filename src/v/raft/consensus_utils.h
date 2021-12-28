fb075f966422ef4e38954707cffe5307e464eb3d
Evgeny Lazin <evgeny@vectorized.io>
2021-12-28 13:09:21 -0500

    raft: Initialize state for pre-existing partitions
    This commit adds new utility function
    'bootstrap_pre_existing_partition'. The function initializes the state
    for the recovered partition. It creates a raft snapshot and updates a
    kv-store value used by raft. The state makes newly created raft group to
    start from the non-zero offset. This is needed to connect newly created
    raft group to restored data and SI data.
    
    Also, the function initializes the offset translator state. The state is
    completely stored inside the kv-store.

--- src/v/raft/consensus_utils.h
+++ src/v/raft/consensus_utils.h
@@ -189,4 +189,19 @@ ss::future<> move_persistent_state(
   ss::shard_id target_shard,
   ss::sharded<storage::api>&);
 
+/// Creates persitent state for pre-existing partition (stored in S3 bucket).
+///
+/// The function is supposed to be called before creating a raft group with the
+/// same group_id. The created group will have 'start_offset' equal to
+/// 'min_kafka_offset' and 'highest_known_offset' equal to 'max_kafka_offset'.
+/// The function will create the offset translator state in kv-store. The offset
+/// translator state will have the right starting point and delta. It will also
+/// create raft snapshot and raft state in kv-store.
+ss::future<> bootstrap_pre_existing_partition(
+  storage::api& api,
+  const storage::ntp_config& ntp_cfg,
+  raft::group_id group,
+  model::offset min_kafka_offset,
+  model::offset max_kafka_offset,
+  std::vector<model::broker> initial_nodes);
 } // namespace raft::details
