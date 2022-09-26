9a705e049ac465103a101bb50de2774ecf6bba57
Evgeny Lazin <evgeny@vectorized.io>
2022-09-26 20:41:01 +0000

    cluster: Extend snapshot_to_manifest tool

--- src/v/cluster/snapshot_to_manifest/snapshot_to_manifest.cc
+++ src/v/cluster/snapshot_to_manifest/snapshot_to_manifest.cc
@@ -12,11 +12,14 @@
 #include "model/fundamental.h"
 #include "model/metadata.h"
 #include "seastarx.h"
+#include "storage/ntp_config.h"
 #include "storage/snapshot.h"
 #include "syschecks/syschecks.h"
+#include "utils/string_switch.h"
 #include "vlog.h"
 
 #include <seastar/core/app-template.hh>
+#include <seastar/core/file-types.hh>
 #include <seastar/core/file.hh>
 #include <seastar/core/fstream.hh>
 #include <seastar/core/future.hh>
@@ -33,8 +36,19 @@
 
 static ss::logger logger{"main"};
 
+enum class operation_type {
+    generate_manifest,
+    generate_snapshot,
+    get_insync_offset,
+};
+
 void cli_opts(boost::program_options::options_description_easy_init opt) {
     namespace po = boost::program_options;
+
+    opt(
+      "operation",
+      po::value<ss::sstring>()->default_value(ss::sstring("generate_manifest")),
+      "Type of the operation");
 
     opt(
       "snapshot",
@@ -57,19 +71,31 @@ void cli_opts(boost::program_options::options_description_easy_init opt) {
     opt("partition", po::value<uint32_t>(), "Partition id");
 
     opt("revision", po::value<uint32_t>(), "Revision id");
+
+    opt("insync", po::value<uint32_t>(), "Snapshot in-sync offset");
 }
 
 struct app_conf {
-    ss::sstring in;
-    ss::sstring out;
+    operation_type type;
+    ss::sstring snapshot_path;
+    ss::sstring manifest_path;
     model::ntp ntp;
     model::initial_revision_id rev;
+    model::offset insync_offset;
 };
+
+static operation_type parse_operation_type(ss::sstring op) {
+    return string_switch<operation_type>(op)
+      .match("manifest", operation_type::generate_manifest)
+      .match("snapshot", operation_type::generate_snapshot)
+      .match("print", operation_type::get_insync_offset);
+}
 
 app_conf cfg_from(boost::program_options::variables_map& m) {
     return app_conf{
-      .in = m["snapshot"].as<ss::sstring>(),
-      .out = m["manifest"].as<ss::sstring>(),
+      .type = parse_operation_type(m["operation"].as<ss::sstring>()),
+      .snapshot_path = m["snapshot"].as<ss::sstring>(),
+      .manifest_path = m["manifest"].as<ss::sstring>(),
       .ntp = model::ntp(
         model::ns(m["namespace"].as<ss::sstring>()),
         model::topic(m["topic"].as<ss::sstring>()),
@@ -120,8 +146,104 @@ public:
         return archival_metadata_stm::manifest_from_snapshot_data(
           std::move(data), std::move(ntp), rev);
     }
+
+    static model::offset get_insync_offset(ss::sstring snapshot_path) {
+        auto path = std::filesystem::path(snapshot_path);
+        auto file = path.filename().string();
+        auto dir = path.remove_filename();
+        storage::simple_snapshot_manager manager(
+          dir, file, ss::default_priority_class());
+        auto opt_reader = manager.open_snapshot().get();
+        if (!opt_reader.has_value()) {
+            vlog(logger.error, "can't open snapshot file");
+            throw std::runtime_error("can't open snapshot file");
+        }
+        auto& reader = *opt_reader;
+        iobuf meta_buf = reader.read_metadata().get();
+        iobuf_parser meta_parser(std::move(meta_buf));
+        auto version = reflection::adl<int8_t>{}.from(meta_parser);
+        vlog(logger.info, "snapshot version {}", version);
+
+        if (version == 0) {
+            vlog(logger.warn, "can't load snapshot, old format");
+            throw std::runtime_error("can't load snapshot, old format");
+        }
+
+        auto hdr_offset = reflection::adl<int64_t>{}.from(meta_parser);
+        auto hdr_version = reflection::adl<int8_t>{}.from(meta_parser);
+        auto hdr_snapshot_size = reflection::adl<int32_t>{}.from(meta_parser);
+        vlog(
+          logger.debug,
+          "stm header offset {}, version {}, size {}",
+          hdr_offset,
+          hdr_version,
+          hdr_snapshot_size);
+        return model::offset(hdr_offset);
+    }
 };
 } // namespace cluster::details
+
+static void generate_manifest_from_snapshot(app_conf cfg) {
+    vlog(
+      logger.info,
+      "input: {}, output: {}",
+      cfg.snapshot_path,
+      cfg.manifest_path);
+    auto manifest
+      = cluster::details::archival_metadata_stm_accessor::load_manifest(
+        cfg.snapshot_path, cfg.ntp, cfg.rev);
+
+    // Serializing the manifest
+    auto [is, size] = manifest.serialize();
+    ss::file outf = ss::open_file_dma(
+                      cfg.manifest_path,
+                      ss::open_flags::create | ss::open_flags::rw)
+                      .get();
+    auto os = ss::make_file_output_stream(outf).get();
+    ss::copy(is, os).get();
+    os.flush().get();
+    os.close().get();
+    is.close().get();
+    vlog(logger.info, "done");
+}
+
+static void generate_snapshot_from_manifest(app_conf cfg) {
+    vlog(
+      logger.info,
+      "input: {}, output: {}",
+      cfg.snapshot_path,
+      cfg.manifest_path);
+    // Load manifest
+    auto file = ss::open_file_dma(cfg.snapshot_path, ss::open_flags::ro).get();
+    cloud_storage::partition_manifest manifest(cfg.ntp, cfg.rev);
+    manifest.update(ss::make_file_input_stream(file)).get();
+    file.close().get();
+    //
+    auto path = std::filesystem::path(cfg.manifest_path);
+    auto fname = path.filename().string();
+    auto dir = path.remove_filename();
+
+    storage::ntp_config ntpc(cfg.ntp, dir.string());
+
+    cluster::archival_metadata_stm::make_snapshot(
+      ntpc, manifest, cfg.insync_offset)
+      .get();
+}
+
+static void dump_snapshot_information(app_conf cfg) {
+    vlog(
+      logger.info,
+      "input: {}, output: {}",
+      cfg.snapshot_path,
+      cfg.manifest_path);
+    auto offset
+      = cluster::details::archival_metadata_stm_accessor::get_insync_offset(
+        cfg.snapshot_path);
+
+    vlog(logger.info, "in-sync offset: {}", offset);
+    // Serializing the manifest
+    vlog(logger.info, "done");
+}
 
 int main(int args, char** argv, char** env) {
     syschecks::initialize_intrinsics();
@@ -132,23 +254,17 @@ int main(int args, char** argv, char** env) {
         vlog(logger.info, "starting");
         auto cfg = cfg_from(app.configuration());
         return ss::async([cfg] {
-            vlog(logger.info, "input: {}, output: {}", cfg.in, cfg.out);
-            auto manifest
-              = cluster::details::archival_metadata_stm_accessor::load_manifest(
-                cfg.in, cfg.ntp, cfg.rev);
-
-            // Serializing the manifest
-            auto [is, size] = manifest.serialize();
-            ss::file outf = ss::open_file_dma(
-                              cfg.out,
-                              ss::open_flags::create | ss::open_flags::rw)
-                              .get();
-            auto os = ss::make_file_output_stream(outf).get();
-            ss::copy(is, os).get();
-            os.flush().get();
-            os.close().get();
-            is.close().get();
-            vlog(logger.info, "done");
+            switch (cfg.type) {
+            case operation_type::generate_manifest:
+                generate_manifest_from_snapshot(cfg);
+                break;
+            case operation_type::generate_snapshot:
+                generate_snapshot_from_manifest(cfg);
+                break;
+            case operation_type::get_insync_offset:
+                dump_snapshot_information(cfg);
+                break;
+            };
         });
     });
 }
