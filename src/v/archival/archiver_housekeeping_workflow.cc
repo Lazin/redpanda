8d8d71a1ccceb98023c22acd5af7049c603ae5c1
Evgeny Lazin <4lazin@gmail.com>
2024-03-31 18:38:42 -0400

    archival: Improve archiver_housekeeping_workflow
    Implement spillover part of the housekeeping. Extract the spillover
    housekeeping into a dedicated method. Keep the implementation simple
    (no boost MSM dependency since the logic is pretty linear and not much
    bookkeeping has to be done).

--- src/v/archival/archiver_housekeeping_workflow.cc
+++ src/v/archival/archiver_housekeeping_workflow.cc
@@ -16,6 +16,7 @@
 #include "archival/logger.h"
 #include "archival/types.h"
 #include "base/outcome.h"
+#include "cloud_storage/types.h"
 #include "config/configuration.h"
 #include "model/fundamental.h"
 #include "ssx/future-util.h"
@@ -57,7 +58,8 @@ public:
           ssx::sformat("{}-{}", _ntp.to_ntp().path(), _id)) {}
 
     ss::future<> start() override {
-        ssx::spawn_with_gate(_gate, [this] { return run_bg_loop(); });
+        ssx::spawn_with_gate(
+          _gate, [this] { return run_bg_loop_until_shutdown(); });
         co_return;
     }
 
@@ -69,13 +71,36 @@ public:
     }
 
 private:
-    ss::future<> run_bg_loop() {
-        auto h = _gate.hold();
+    ss::future<> run_bg_loop_until_shutdown() {
         while (true) {
+        size_t prev_num_delete_requests = 0;
+        std::optional<std::error_code> prev_errc;
+        while (!_as.abort_requested()) {
+            // The loop is repeating the following steps:
+            // - if archiver_housekeeping was started by the scheduler
+            //   - Apply retention to spillover region
+            //   - Apply GC to spillover region
+            //   - Apply GC to the region of the log managed by the STM
+            // - if stm_housekeeping was started by the scheduler
+            //   - Apply retention to the region of the log managed by the STM
+            //   - Apply GC to the region of the log managed by the STM
+            //
+            // Every step could potentially replicate a command. Every step
+            // returns information about the STM state (its read-write fence
+            // offset). This information is carried over to the next step so it
+            // could use the offset to replicate its own command.
+
+            // We're passing the number of delete requests from the previous
+            // step or the error code if the previous iteration failed. This is
+            // needed in order for the scheduler to be able to throttle the
+            // housekeeping or prioritize it in some cases.
             archiver_scheduler_api::suspend_housekeeping_arg suspend_req{
               .ntp = _ntp,
-              .num_delete_requests_used = 0, // TODO: propagate value from GC
+              .num_delete_requests_used = prev_num_delete_requests,
+              .manifest_dirty = manifest_dirty,
+              .errc = prev_errc,
             };
+            prev_num_delete_requests = 0;
             auto suspend_res = co_await _scheduler->maybe_suspend_housekeeping(
               suspend_req);
             if (
@@ -92,24 +117,94 @@ private:
                   suspend_res.error());
                 co_return;
             }
+            switch (suspend_res.value().type) {
+                auto res = co_await perform_stm_housekeeping(
+                  next_housekeeping_action_type;
+            case stm_housekeeping: {
+                auto res = co_await perform_archive_housekeeping(
+                  suspend_res.value().requests_quota);
+                if (res.has_error()) {
+                    auto errc = res.error();
+                    if (
+                        vlog(
+                          _log.debug,
+                          "Shutting down STM housekeeping operation");
+                      || errc == cloud_storage::error_outcome::shutting_down) {
+                        // Shutting down gracefully
+                        vlog(_log.debug, "Shutting down housekeeping workflow");
+                        co_return;
+                    }
+                    // Otherwise the errc is a recoverable error
+                    // cycle back to the scheduler
+                    prev_errc = errc;
+                    prev_num_delete_requests = 0;
+                    manifest_dirty = false;
+                    continue;
+                } else {
+                    prev_errc = {};
+                    prev_num_delete_requests = res.value().quota_used;
+                    manifest_dirty = true;
+                }
+            } break;
+            case archive_housekeeping:
+                vassert(false, "Not implemented");
+                break;
+            }
+        }
+        co_return;
+    }
+
+    struct archive_housekeeping_result {
+        size_t quota_used{0};
+    };
+
+    // Apply retention and GC to the spillover region of the log
+    ss::future<result<archive_housekeeping_result>>
+    perform_archive_housekeeping(size_t request_quota) noexcept {
+        try {
+            // Apply retention
             archiver_operations_api::apply_archive_retention_arg apply_arch_req{
               .ntp = _ntp,
-              .delete_op_quota = 1000, // TODO: use value from the config
+              .delete_op_quota = request_quota,
             };
             auto apply_arch_res = co_await _operations->apply_archive_retention(
               _rtc, apply_arch_req);
             if (apply_arch_res.has_error()) {
-                // Recoverable error
                 vlog(
                   _log.warn,
                   "Housekeeping error (apply archive retention): {}",
                   apply_arch_res.error());
-                continue;
+                co_return apply_arch_res.error();
             }
-            // TODO: add remaining housekeeping ops
+            // Run archive GC
+            auto arch_gc_res = co_await _operations->garbage_collect_archive(
+              _rtc, apply_arch_res.value());
+            if (arch_gc_res.has_error()) {
+                vlog(
+                  _log.warn,
+                  "Housekeeping error (archive GC): {}",
+                  arch_gc_res.error());
+                co_return arch_gc_res.error();
+            }
+            // Run STM GC to remove replaced segments.
+            // Without this step replaced segments will accumulate in the
+            // manifest. We will run out of memory eventually.
+            auto gc_res = co_await _operations->garbage_collect(
+              _rtc,
+              archiver_operations_api::apply_stm_retention_result{
+                .ntp = _ntp,
+                .segments_removed
+                = arch_gc_res.value().num_replaced_segments_to_remove,
+                .read_write_fence = arch_gc_res.value().read_write_fence,
+              });
+            co_return archive_housekeeping_result{
+              .quota_used = arch_gc_res.value().num_delete_requests};
+        } catch (...) {
+            _fatal = std::current_exception();
+            co_return error_outcome::shutting_down;
         }
-        co_return;
     }
+
     model::ktp _ntp;
     model::term_id _id;
     ss::shared_ptr<archiver_operations_api> _operations;
